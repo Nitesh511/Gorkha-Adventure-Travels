f858ce2d44eae15bc31d5d0b1b179420cf75a7b8
unknown <nitesh.karki31@gmail.com>
2024-02-27 22:13:06 +0545

    commit for products show in the homepage using redux

--- frontend/Gorkhas_Adventure_Treks/src/components/Home/homepage.jsx
+++ frontend/Gorkhas_Adventure_Treks/src/components/Home/homepage.jsx
@@ -1,26 +1,37 @@
 import React, { useEffect } from 'react';
 import video from "../../assets/video.mp4";
 import { getProduct } from '../../actions/productActions';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Products from "../Home/product";
 
 const Homepage = () => {
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector(state => state.products);
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getProduct());
-  },[dispatch]);
+  }, [dispatch]);
+
   return (
-    <div className='relative w-full h-full'>
+    <div className=''>
+      <div className=''>
+        <video className='w-full h-screen object-cover' autoPlay muted loop>
+          <source src={video} type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-3xl font-bold">
-        <p className="mb-4">Lets Explore The Nature Together</p>
+        <p className="mb-4">Let's Explore The Nature Together</p>
         <button type='button' className="text-white bg-gradient-to-br from-green-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
           Explore
         </button>
       </div>
-      <video className='w-full h-screen object-cover' autoPlay muted loop>
-        <source src={video} type='video/mp4' />
-        Your browser does not support the video tag.
-      </video>
+      <h2 className='text-4xl'>All Packages</h2>
+      <div className='grid grid-cols-4 gap-4 containers' id='containers'>
+        {products && products.map((product) =>
+          <Products key={product.id} product={product} />
+        )}
+      </div>
     </div>
   );
 };
---
+++ frontend/Gorkhas_Adventure_Treks/src/components/Home/product.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Products = ({ product }) => {
+  return (
+    <div className="p-4">
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <Link to={`/product/${product.id}`}>
+            <img className="p-8 rounded-t-lg" src="https://cdn.mos.cms.futurecdn.net/D9bzCVeZLHQnZ6bUWvAkrW-1200-80.jpg" alt="product image" />
+          </Link>
+          <div className="px-5 pb-5">
+            <Link to={`/product/${product.id}`}>
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
+            </Link>
+            <p className="text-gray-700 dark:text-gray-300 mt-2">{product.description}</p>
+            <div className="flex items-center justify-between mt-4">
+              <span className="text-xl font-bold text-gray-900 dark:text-white">Rs: {product.price}</span>
+              <Link to={`/product/${product.id}`} className="text-blue-700 hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-600">View Details</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Products;
